7956faf65f532447b9de09f9b017444aa3604e96
Reddy, Himananda <himananda.reddy@mastercard.com>
2017-03-05 19:34:09 +0530

    Initial commit

---
+++ src/main/java/com/hreddy/App.java
@@ -0,0 +1,13 @@
+package com.hreddy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
